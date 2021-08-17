5a5c2b828f42e33d0c7b6587be5d418ee5fb46cc
Semyeong Hong <ca711207@gmail.com>
2021-08-17 21:49:44 +0900

    declared database with postgreSql

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db("DATABASE_URL", default="postgres:///djangostagram"),
+    "default": env.db("DATABASE_URL", default="postgres://postgres:Ghdtpauddlek1@@localhost:5432/djangostagram"),
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
