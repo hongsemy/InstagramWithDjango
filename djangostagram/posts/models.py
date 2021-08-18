1ca109f0cd965fd33a769b01f1014956e114f95d
Semyeong Hong <ca711207@gmail.com>
2021-08-18 21:20:06 +0900

    Database

---
+++ djangostagram/posts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from Djangostagram.users import models as user_model
+
+# Create your models here.
+
+# This class is used in other models as an inheritance.
+# An often-used pattern 
+class TimeStamedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    # An option that makes this model to not show up directly on the database
+    class Meta:
+        abstract = True
+
+
+class Posts(TimeStamedModel):
+    author = models.ForeignKey(
+                user_model.User,
+                null = True,
+                on_delete = models.CASCADE,
+                related_name = "post_author"
+            )
+    caption = models.TextField(blank=True)
+    image = models.ImageField(blank=True)
+    image_likes = models.ManyToManyField(user_model.User, related_name='post_image_likes')
+
+
+class Comments(TimeStamedModel):
+    author = models.ForeignKey(
+                user_model.User,
+                null = True,
+                on_delete = models.CASCADE,
+                related_name = "comment_author"
+            )
+    posts = models.ForeignKey(
+                Posts,
+                null = True,
+                on_delete = models.CASCADE
+                related_name = "comment_post"
+    )
+    contents = TextField(blank=True)
--- djangostagram/users/models.py
+++ djangostagram/users/models.py
@@ -1,16 +1,33 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     """Default user for Djangostagram."""
+    GENDER_CHOICES = [
+        ('M', 'MALE'),
+        ('F', 'FEMALE'),
+        ('C', 'Custom'),     
+        # Users will see the second element in each tuple (e.g. MALE) 
+        # and db will save the value of the first element in each tuple (e.g. M).
+    ]   
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    user_name = models.CharField(blank=True, max_length=255)
+    profile_pic = models.ImageField(blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    email = models.CharField(blank=True, max_length=255)
+    phone_number = models.CharField(blank=True, max_length=255)
+    gender = models.CharField(blank=True, choices = GENDER_CHOICES, max_length=255)
+    # parameter choices will limit the user to choose only from the given choices 
+    # based on what is passed as an argument.
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
+
 
     def get_absolute_url(self):
         """Get url for user's detail view.
